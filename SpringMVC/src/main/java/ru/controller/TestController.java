2a024dcf954284048f83f4a821a3ab22e6ecfb73
Бачев Константин Николаевич <Bachev.K.N@sberbank.ru>
2022-01-16 16:53:12 +0300

    added new requestMapping with used @RequestParam annotation

--- SpringMVC/src/main/java/ru/controller/TestController.java
+++ SpringMVC/src/main/java/ru/controller/TestController.java
@@ -3,6 +3,7 @@ package ru.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,6 +23,12 @@ public class TestController {
     public String processForm_2(HttpServletRequest request, Model model) {
         String name = request.getParameter("carName");
         model.addAttribute("message","Congratulation, you are won the car " + name.toUpperCase());
+        return "processForm";
+    }
+
+    @RequestMapping("/processForm3")
+    public String processForm_3(@RequestParam("carName") String name, Model model) {
+        model.addAttribute("message","Congratulation, you are won the NEW car " + name.toUpperCase());
         return "processForm";
     }
 }
