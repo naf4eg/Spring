4cef571f496644c52283ae9576cbb8b15dcc8b05
Бачев Константин Николаевич <Bachev.K.N@sberbank.ru>
2022-02-10 22:01:26 +0300

    added model Driver on car controller

--- SpringMVC/src/main/java/ru/controller/car/CarController.java
+++ SpringMVC/src/main/java/ru/controller/car/CarController.java
@@ -5,20 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.controller.car.model.Car;
+import ru.controller.car.model.Driver;
 
 @Controller
 @RequestMapping("/car")
 public class CarController {
+    private static final String CAR = "car";
+    private static final String DRIVER = "DRIVER";
 
     @RequestMapping("/showRegistrationForm")
     String showRegistrationForm(Model model){
-        model.addAttribute("car", new Car());
+        model.addAttribute(CAR, new Car());
+        model.addAttribute(DRIVER, new Driver());
         return "car-registration-form";
     }
 
     @RequestMapping("/showRegisteredForm")
-    String showRegisteredForm(@ModelAttribute("car") Car theCar) {
+    String showRegisteredForm(
+            @ModelAttribute(CAR) Car theCar,
+            @ModelAttribute(DRIVER) Driver theDriver
+    ) {
         System.out.println(theCar.getModelName() + " " + theCar.getColor());
+        System.out.println(theDriver.getFirstName() + " " + theDriver.getLastName());
         return "car-registered-form";
     }
 }
