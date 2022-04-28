2f4e48f5b4ddfbe0e24382e3c087c69e223d6746
Konstantin <naf4eg@yandex.ru>
2022-04-28 14:35:11 +0300

    SpringAOP - JoinPoint : get args the method

--- SpringAOP/src/main/java/ru/aop/example/aspect/joinpoint/JoinPointAspect.java
+++ SpringAOP/src/main/java/ru/aop/example/aspect/joinpoint/JoinPointAspect.java
@@ -5,12 +5,15 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import ru.aop.example.model.Employee;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
 public class JoinPointAspect {
 
-    @Pointcut("execution(* ru.aop.example.dao5.*.get*())")
+    @Pointcut("execution(* ru.aop.example.dao5.*.set*(..))")
     private void pc() {}
 
     @Before("pc()")
@@ -19,5 +22,9 @@ public class JoinPointAspect {
 
         var signature = joinPoint.getSignature();
         System.out.println("call method: " + signature);
+
+        Object[] args = joinPoint.getArgs();
+
+        Arrays.stream(args).forEach(a -> System.out.println("arg print: " + a));
     }
 }
--- SpringAOP/src/main/java/ru/aop/example/dao5/EmployeeDAO.java
+++ SpringAOP/src/main/java/ru/aop/example/dao5/EmployeeDAO.java
@@ -1,17 +1,18 @@
 package ru.aop.example.dao5;
 
 import org.springframework.stereotype.Component;
+import ru.aop.example.model.Employee;
 
 @Component
 public class EmployeeDAO {
 
-    private String name;
+    private Employee employee;
 
-    public String getName() {
-        return name;
+    public Employee getEmployee() {
+        return employee;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
     }
 }
