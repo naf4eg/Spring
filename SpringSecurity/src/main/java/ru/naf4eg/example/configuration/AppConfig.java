3999cd484ecacb86180196df5217b20c9dd0e147
Konstantin <naf4eg@yandex.ru>
2022-05-25 22:04:38 +0300

    Spring Security - new project

---
+++ SpringSecurity/src/main/java/ru/naf4eg/example/configuration/AppConfig.java
@@ -0,0 +1,12 @@
+package ru.naf4eg.example.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ru.naf4eg.example")
+public class AppConfig {
+
+}
