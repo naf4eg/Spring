7ec9c601b59bb82b801aea0534c8fc72c53b5c9d
Бачев Константин Николаевич <Bachev.K.N@sberbank.ru>
2022-06-01 18:03:45 +0300

    Restrict Access - added access denied configuration and access-denied.jsp page

--- SpringSecurity/src/main/java/ru/naf4eg/example/controller/LoginController.java
+++ SpringSecurity/src/main/java/ru/naf4eg/example/controller/LoginController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
     public static final String LOGIN_PAGE_URL = "/loginPage";
+    public static final String ACCESS_DENIED_URL = "/access-denied";
 
     @RequestMapping(LOGIN_PAGE_URL)
     String getLoginPage() {
         return "fancy-login";
+    }
+
+    @RequestMapping(ACCESS_DENIED_URL)
+    String getAccessDenied() {
+        return "access-denied";
     }
 }
---
+++ SpringSecurity/src/main/webapp/WEB-INF/view/access-denied.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+	<title>Access Denied Page</title>
+</head>
+<body>
+<h2>Access Denied Page</h2>
+<hr>
+<p>
+	Access Denied!!!
+</p>
+<hr>
+<a href="${pageContext.request.contextPath}\">Back to Home Page</a>
+
+</body>
+</html>
