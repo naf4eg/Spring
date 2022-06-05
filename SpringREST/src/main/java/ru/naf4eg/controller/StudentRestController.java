ba0ba27e99ce45bd87c53e12f88c2413fc1e59df
Konstantin <naf4eg@yandex.ru>
2022-06-05 20:01:38 +0300

    Spring REST - Exception handling - added logic for throw exception in Rest controller

--- SpringREST/src/main/java/ru/naf4eg/controller/StudentRestController.java
+++ SpringREST/src/main/java/ru/naf4eg/controller/StudentRestController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.naf4eg.model.Student;
+import ru.naf4eg.model.StudentNotFoundException;
 
 import java.util.List;
 
@@ -18,6 +19,9 @@ public class StudentRestController {
 
     @RequestMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if (studentId >= Student.getStudents().size() || studentId < 0) {
+            throw new StudentNotFoundException("Student is not found");
+        }
         return Student.getStudents().get(studentId);
     }
 }
