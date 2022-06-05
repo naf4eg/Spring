57511f3cc8a9aad84c30733b34031f3a0576d8cf
Konstantin <naf4eg@yandex.ru>
2022-06-05 20:07:17 +0300

    Spring REST - Exception handling - added Exception Handler

--- SpringREST/src/main/java/ru/naf4eg/controller/StudentRestController.java
+++ SpringREST/src/main/java/ru/naf4eg/controller/StudentRestController.java
@@ -1,9 +1,13 @@
 package ru.naf4eg.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.naf4eg.model.Student;
+import ru.naf4eg.model.StudentErrorResponse;
 import ru.naf4eg.model.StudentNotFoundException;
 
 import java.util.List;
@@ -23,5 +27,14 @@ public class StudentRestController {
             throw new StudentNotFoundException("Student is not found");
         }
         return Student.getStudents().get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception) {
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setMessage(exception.getMessage());
+        errorResponse.setTimestamp(System.currentTimeMillis());
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<StudentErrorResponse>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
