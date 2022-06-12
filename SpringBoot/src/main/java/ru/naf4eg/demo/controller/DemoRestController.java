db3dc303b21ecf94682752b01125c3ba20f2e600
Konstantin <naf4eg@yandex.ru>
2022-06-12 19:59:16 +0300

    Spring Boot

---
+++ SpringBoot/src/main/java/ru/naf4eg/demo/controller/DemoRestController.java
@@ -0,0 +1,16 @@
+package ru.naf4eg.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
+@RestController
+public class DemoRestController {
+
+    @GetMapping("/")
+    String sayHello() {
+        return "Hello World " + "\n" + LocalDateTime.now() + "\n" + OffsetDateTime.now();
+    }
+}
